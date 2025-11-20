9e28a818c7c99ccec9b9a322a3b73d59d7dcacdd
Harsh Gaur <hgaur701@gmail.com>
2025-11-21 04:46:31 +0530

    made block button functional

---
+++ background.js
@@ -0,0 +1,12 @@
+//chrome.tabs.onActivated.addListner(redirectToPandaStopPage);
+
+//async function redirectToPandaStopPage(activeInfo) {
+
+//	try {
+//		console.log('activeInfo: ', activeInfo);
+
+//	} catch (error) {
+
+//	}
+//}
+
--- popup.js
+++ popup.js
@@ -1,17 +1,52 @@
-// Navigation between popup.html and option.html
+// ./popup.js
+
+// Add the website's hostname to the local storage.
+function blockWebsite(hostname) {
+	chrome.storage.local.get('Panda_police_data', (result) => {
+		if (!result.Panda_police_data) {
+			chrome.storage.local.set({ 'Panda_police_data': { blockedwebsites: [] } },
+				function() {
+					console.log('Storage did not exist, Insitialized it');
+					const blockedWebsites = result.Panda_police_data?.blockedWebsites || [];
+					blockedWebsites[0] = hostname;
+					chrome.storage.local.set({ 'Panda_police_data': { 'blockedWebsites': blockedWebsites } })
+				}
+			)
+			return;
+		}
+		else {
+			const blockedWebsites = result.Panda_police_data?.blockedWebsites;
+			if(blockedWebsites.includes(hostname)){
+				console.log('Already blocked');
+				return;
+			}
+			blockedWebsites.push(hostname);
+			chrome.storage.local.set({ 'Panda_police_data': { 'blockedWebsites': blockedWebsites } })
+		}
+	})
+}
 
 // Check if back button exists (on option.html) and navigate to popup.html
 const backBtn = document.getElementById('backBtn');
 if (backBtn) {
-    backBtn.addEventListener('click', function () {
-        window.location.href = 'popup.html';
-    });
+	backBtn.addEventListener('click', function() {
+		window.location.href = 'popup.html';
+	});
 }
 
 // Add listener for buttons that navigate to option.html
 const openOptionsButtons = document.querySelectorAll('.open-options');
 openOptionsButtons.forEach(btn => {
-    btn.addEventListener('click', function () {
-        window.location.href = 'option.html';
-    });
+	btn.addEventListener('click', function() {
+		window.location.href = 'option.html';
+	});
 });
+
+const onBlockButtonClick = document.querySelector('.block-button');
+onBlockButtonClick.addEventListener('click', function() {
+	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+		const newUrl = new URL(tabs[0].url);
+		blockWebsite(newUrl.hostname);
+	});
+})
+
