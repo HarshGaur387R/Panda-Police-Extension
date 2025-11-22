883ab6ab6019ee4e9a8e342c18c62ec2a916c1a6
Harsh Gaur <hgaur701@gmail.com>
2025-11-22 06:07:10 +0530

    block button is funtional

--- popup.js
+++ popup.js
@@ -1,7 +1,10 @@
 // ./popup.js
+// TODO: Change text and make block-button dissable once website is dissable.
+
+const blockButton = document.querySelector('.block-button');
 
 // Add the website's hostname to the local storage.
-function blockWebsite(hostname) {
+function blockWebsite(hostname, tabId) {
 	chrome.storage.local.get('Panda_police_data', (result) => {
 		if (!result.Panda_police_data) {
 			chrome.storage.local.set({ 'Panda_police_data': { blockedwebsites: [] } },
@@ -10,21 +13,25 @@ function blockWebsite(hostname) {
 					const blockedWebsites = result.Panda_police_data?.blockedWebsites || [];
 					blockedWebsites[0] = hostname;
 					chrome.storage.local.set({ 'Panda_police_data': { 'blockedWebsites': blockedWebsites } })
+					chrome.tabs.update(tabId, { url: chrome.runtime.getURL("block-page.html") });
 				}
 			)
 			return;
 		}
 		else {
 			const blockedWebsites = result.Panda_police_data?.blockedWebsites;
-			if(blockedWebsites.includes(hostname)){
+			if (blockedWebsites.includes(hostname)) {
 				console.log('Already blocked');
 				return;
 			}
 			blockedWebsites.push(hostname);
 			chrome.storage.local.set({ 'Panda_police_data': { 'blockedWebsites': blockedWebsites } })
+			chrome.tabs.update(tabId, { url: chrome.runtime.getURL("block-page.html") });
 		}
 	})
 }
+
+// // // // // DOM Manipulation // // // // // 
 
 // Check if back button exists (on option.html) and navigate to popup.html
 const backBtn = document.getElementById('backBtn');
@@ -42,11 +49,14 @@ openOptionsButtons.forEach(btn => {
 	});
 });
 
-const onBlockButtonClick = document.querySelector('.block-button');
-onBlockButtonClick.addEventListener('click', function() {
-	chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-		const newUrl = new URL(tabs[0].url);
-		blockWebsite(newUrl.hostname);
-	});
-})
 
+if (blockButton) {
+	blockButton.addEventListener('click', function() {
+		chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+			const newUrl = new URL(tabs[0].url);
+			if (newUrl.protocol === 'https:') {
+				blockWebsite(newUrl.hostname, tabs[0].tabId);
+			}
+		});
+	})
+}
