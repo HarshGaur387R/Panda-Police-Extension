20bf6c3309ed7e7be958e482565ac93bfe59d412
Harsh Gaur <hgaur701@gmail.com>
2025-11-22 06:08:14 +0530

    Added event in service to listen for tabs changes

--- background.js
+++ background.js
@@ -1,12 +1,19 @@
-//chrome.tabs.onActivated.addListner(redirectToPandaStopPage);
-
-//async function redirectToPandaStopPage(activeInfo) {
-
-//	try {
-//		console.log('activeInfo: ', activeInfo);
-
-//	} catch (error) {
-
-//	}
-//}
-
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+	if (changeInfo && changeInfo.url) {
+		const changedUrl = new URL(changeInfo.url);
+		if (changedUrl.protocol !== 'https:') {
+			console.log('protocol is not https: ');
+			return;
+		} else {
+			chrome.storage.local.get('Panda_police_data', (result) => {
+				if (result) {
+					const blockedWebsites = result.Panda_police_data?.blockedWebsites || [];
+					if (blockedWebsites.includes(changedUrl.hostname)) {
+						chrome.tabs.update(tabId, { url: chrome.runtime.getURL("block-page.html") });
+					}
+				}
+			})
+			console.log("Tab URL changed to:", changeInfo.url);
+		}
+	}
+});
