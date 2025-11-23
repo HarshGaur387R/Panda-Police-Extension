eb337b1da457606c8ac196439f3306bc6d060258
Harsh Gaur <hgaur701@gmail.com>
2025-11-23 22:52:36 +0530

    a change

--- popup.js
+++ popup.js
@@ -33,13 +33,7 @@ function blockWebsite(hostname, tabId) {
 
 // // // // // DOM Manipulation // // // // // 
 
-// Check if back button exists (on option.html) and navigate to popup.html
-const backBtn = document.getElementById('backBtn');
-if (backBtn) {
-	backBtn.addEventListener('click', function() {
-		window.location.href = 'popup.html';
-	});
-}
+
 
 // Add listener for buttons that navigate to option.html
 const openOptionsButtons = document.querySelectorAll('.open-options');
